8fa51aca8d987c0f1edc5b25c38c2eeacffe0863
Zachary Johnson <51001850+k4m1@users.noreply.github.com>
2021-02-14 13:14:51 -0500

    added category tiles 2-5
    added sports health science technology tiles

--- lib/helper/data.dart
+++ lib/helper/data.dart
@@ -5,10 +5,48 @@ List<CategoryModel> getCategories() {
   CategoryModel categoryModel = new CategoryModel();
 
   // bussiness
-  categoryModel.categoryName = Bussiness;
+  categoryModel.categoryName = "Bussiness";
   categoryModel.imageUrl =
       "https://images.unsplash.com/photo-1519748174344-16e5d53bda7a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80";
   category.add(categoryModel);
 
   categoryModel = new CategoryModel();
+
+  // entertainment
+  CategoryModel = new CategoryModel();
+  CategoryModel.categoryName = "Entertainment";
+  CategoryModel.imageUrl = "https://images.unsplash.com/photo-1522869635100-9f4c5e86aa37?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80";
+  category.add(CategoryModel);
+
+  // general
+  CategoryModel = new CategoryModel();
+  CategoryModel.categoryName = "General";
+  CategoryModel.imageUrl = "https://images.unsplash.com/photo-1495020689067-958852a7765e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
+  category.add(CategoryModel);
+
+  // health
+  CategoryModel = new CategoryModel();
+  CategoryModel.categoryName = "Health";
+  CategoryModel.imageUrl = "https://images.unsplash.com/photo-1494390248081-4e521a5940db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1595&q=80";
+  category.add(CategoryModel);
+
+  //science
+  CategoryModel = new CategoryModel();
+  CategoryModel.categoryName = "Science";
+  CategoryModel.imageUrl = "https://images.unsplash.com/photo-1554475901-4538ddfbccc2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80";
+  category.add(CategoryModel);
+
+  //sports
+  CategoryModel = new CategoryModel();
+  CategoryModel.categoryName = "Sports";
+  CategoryModel.imageUrl = "https://images.unsplash.com/photo-1495563923587-bdc4282494d0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80";
+  category.add(CategoryModel);
+
+  //5
+  CategoryModel = new CategoryModel();
+  CategoryModel.categoryName = "Technology";
+  CategoryModel.imageUrl = "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80";
+  category.add(CategoryModel);
+
+  return category;
 }
