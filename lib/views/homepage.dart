f42d5501655f8b6cefaa6fa34335295c83c25c5f
Zachary Johnson <51001850+k4m1@users.noreply.github.com>
2021-02-14 12:34:26 -0500

    update homepage widget
    added scffold and basic styling to homepage, also added the ground work for the category tiles ontop

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -10,8 +10,39 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+         title: Row(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: <Widget>[
+             Text("Flutter"),
+             Text("News", style: TextStyle(
+               color: Colors.blue
+             ),)
+           ],
+         ),
+         centerTitle: true,
+         elevation: 0.0,
+       ),
+       body: Container(
+         child: ,
+       ),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+
+  final imageURL, categoryName;
+  CategoryTile({this.imageURL, this.categoryName})
+  @override
+  Widget build(BuildContext context) {
     return Container(
-       child: child,
+      child: Stack(
+        children: <Widget[
+          Image.network(imageURL, width: 120, height: 60,),
+        ],
+      ),
     );
   }
 }
