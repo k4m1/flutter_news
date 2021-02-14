3bdbed54e389bc48d10981b598301a9804c72b9b
Zachary Johnson <51001850+k4m1@users.noreply.github.com>
2021-02-14 13:49:38 -0500

    fixed data helper typos
    CatagoryModel was being used on accident instead of categoryModel

--- lib/helper/data.dart
+++ lib/helper/data.dart
@@ -13,40 +13,40 @@ List<CategoryModel> getCategories() {
   categoryModel = new CategoryModel();
 
   // entertainment
-  CategoryModel = new CategoryModel();
-  CategoryModel.categoryName = "Entertainment";
-  CategoryModel.imageUrl = "https://images.unsplash.com/photo-1522869635100-9f4c5e86aa37?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80";
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Entertainment";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1522869635100-9f4c5e86aa37?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80";
   category.add(categoryModel);
 
   // general
-  CategoryModel = new CategoryModel();
-  CategoryModel.categoryName = "General";
-  CategoryModel.imageUrl = "https://images.unsplash.com/photo-1495020689067-958852a7765e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "General";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1495020689067-958852a7765e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
   category.add(categoryModel);
 
   // health
-  CategoryModel = new CategoryModel();
-  CategoryModel.categoryName = "Health";
-  CategoryModel.imageUrl = "https://images.unsplash.com/photo-1494390248081-4e521a5940db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1595&q=80";
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Health";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1494390248081-4e521a5940db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1595&q=80";
   category.add(categoryModel);
 
   //science
-  CategoryModel = new CategoryModel();
-  CategoryModel.categoryName = "Science";
-  CategoryModel.imageUrl = "https://images.unsplash.com/photo-1554475901-4538ddfbccc2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80";
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Science";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1554475901-4538ddfbccc2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80";
   category.add(categoryModel);
 
   //sports
-  CategoryModel = new CategoryModel();
-  CategoryModel.categoryName = "Sports";
-  CategoryModel.imageUrl = "https://images.unsplash.com/photo-1495563923587-bdc4282494d0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80";
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Sports";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1495563923587-bdc4282494d0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80";
   category.add(categoryModel);
 
-  //5
-  CategoryModel = new CategoryModel();
-  CategoryModel.categoryName = "Technology";
-  CategoryModel.imageUrl = "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80";
-  category.add(categoryModel);
+    //5
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Technology";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80";
+    category.add(categoryModel);
 
   return category;
 }
